e541f62abca548f2a7bff29ed93fa074889b32fe
ZarryMyles <mahanth2709@gmail.com>
2022-12-21 01:26:10 +0530

    Configurations and scripts

---
+++ components/Landing.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Landing = () => {
+  return (
+    <section id="hero" className="min-h-screen container mx-auto">
+      <div className="hero-title invisible"></div>
+    </section>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#C4C4C4",
+        primary: "#DE3C3F",
+        dark: "#171717",
+        "light-text": "#E3E6E8",
+        gray: "#DCE0E3",
+        "dark-gray": "#222222",
+        "footer-gray": "#808080",
+      },
+    },
   },
   plugins: [],
 };
