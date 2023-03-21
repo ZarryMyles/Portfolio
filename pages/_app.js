f34ca47115504c17d15b70cd099d2f188521b125
ZarryMyles <mahanth2709@gmail.com>
2023-03-22 03:00:02 +0530

    Secured GA IDs

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,24 @@ import "../styles/globals.css";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+          `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default function Document() {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MPLKDTW');`,
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}');`,
           }}
         ></Script>
       </Head>
@@ -21,10 +21,12 @@ export default function Document() {
         {/* Google Tag Manager (noscript) */}
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPLKDTW"
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         ></noscript>
+
+        {/* Main Application Tags */}
         <Main />
         <NextScript />
       </body>
