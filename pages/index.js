90b8a5d57273e65fdaa415bbb1507e0df4cd75a7
ZarryMyles <mahanth2709@gmail.com>
2023-07-16 21:55:10 +0530

    Updated SEO and Meta Tags

--- pages/index.js
+++ pages/index.js
@@ -42,15 +42,20 @@ export default function Home() {
     <>
       <Head>
         {/* Meta Tags */}
-        <title>Mahanth | Developer</title>
-        <meta name="keywords" content="ZarryMyles, Mahantha, React" />
-        <meta name="description" content="Mahantha N | Web Developer" />
+        <title>Mahanth - Web Developer</title>
+        <meta name="keywords" content="ZarryMyles, Mahantha N" />
+        <meta
+          name="description"
+          content="A MERN stack Developer based in Bangalore who builds websites focused on simplicity and performance."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta name="author" content="Mahantha N" />
 
-        {/* Open Graph */}
-        <meta property="og:url" content="https://www.mahanth.dev" />
+        {/* HTML Meta Tags */}
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://mahanth.dev" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Mahantha - Web Developer" />
         <meta
@@ -59,12 +64,13 @@ export default function Home() {
         />
         <meta
           property="og:image"
-          itemprop="image"
           content="https://res.cloudinary.com/zarry/image/upload/v1671608478/profile_zuk8mn.png"
         />
+
+        {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="mahanth.dev" />
-        <meta property="twitter:url" content="https://www.mahanth.dev/" />
+        <meta property="twitter:url" content="https://mahanth.dev" />
         <meta name="twitter:title" content="Mahantha - Web Developer" />
         <meta
           name="twitter:description"
@@ -74,6 +80,8 @@ export default function Home() {
           name="twitter:image"
           content="https://res.cloudinary.com/zarry/image/upload/v1671608478/profile_zuk8mn.png"
         />
+
+        {/* Meta Tags Generated via https://www.opengraph.xyz */}
 
         {/* Favicon */}
         <link
